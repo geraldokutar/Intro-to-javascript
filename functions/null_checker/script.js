09005d56f0c76a43e6bbd18bb18c5e8c499bb902
geraldokutar <geraldpilk@gmail.com>
2019-03-14 16:04:50 +0300

    Add null_chacker as function

--- functions/null_checker/script.js
+++ functions/null_checker/script.js
@@ -1,9 +1,11 @@
-var number = 10;
+const nulls = number => {
 
-if (number > 0) {
-  console.log(1);
-} else if (number < 0) {
-  console.log(-1);
-} else if (number == 0) {
-  console.log(0);
+  if (number > 0) {
+    console.log(1);
+  } else if (number < 0) {
+    console.log(-1);
+  } else if (number == 0) {
+    console.log(0);
+  }
 }
+console.log(nulls(10));
