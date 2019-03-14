79a2656290e6f7f7a662c07bd837ef5b6224ddb8
geraldokutar <geraldpilk@gmail.com>
2019-03-14 16:03:48 +0300

    Add fizzBuzz as a function

--- functions/fizzBuzz.js
+++ functions/fizzBuzz.js
@@ -1,15 +1,17 @@
-var x = 1;
-while(x <= 100){
-  if (x%3===0&& x%5===0) {
-    console.log("FizzBuzz");
+const fizz = x => {
+  while(x <= 100){
+    if (x%3===0&& x%5===0) {
+      console.log("FizzBuzz");
 
-  }else if (x%3===0) {
-    console.log(" Fizz");
-  }else if (x%5===0) {
-    console.log("Buzz");
+    }else if (x%3===0) {
+      console.log(" Fizz");
+    }else if (x%5===0) {
+      console.log("Buzz");
 
-  }else{
-    console.log(x);
+    }else{
+      console.log(x);
+    }
+    x++
   }
-  x++
 }
+console.log(fizz(12));
