86f5edc6201bc8b5daf32ab5f49f57d9586f095a
geraldokutar <geraldpilk@gmail.com>
2019-03-14 16:05:49 +0300

    Add prime_logger as a js function

--- functions/prime_logger.js
+++ functions/prime_logger.js
@@ -1,9 +1,14 @@
-var n = 10;
-
-nextPrime:
-for (let i = 2; i <= n; i++) {
-  for (let j = 2; j < i; j++) {
-    if (i % j == 0) continue nextPrime;
+const prime = n => {
+  for (var num= 2; num <= n; num++) {
+    var notPrime = false;
+    for (var i = 2; i <= num; i++) {
+      if(num%i===0 && i!==num) {
+        notPrime = true;
+      }
+    }
+    if(notPrime === false) {
+      console.log(num);
+    }
   }
-  console.log(i);
 }
+console.log(prime(100));
